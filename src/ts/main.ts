80bfcbc6622ab74e123d5073a841b432d055beb3
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2024-05-26 23:47:58 +0100

    add initial functionality

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,52 +1,99 @@
 import "../css/tailwind.css";
+import { Cursor } from "./types/types";
 
 export const wordsArray: string[] = [
-    "Apple",
-    "Basic",
-    "Blade",
-    "Bread",
-    "Cloud",
-    "Dance",
-    "Flute",
-    "Grape",
-    "Image",
-    "Knife",
-    "Lemon",
-    "Mouse",
-    "Nurse",
-    "Ocean",
-    "Paste",
-    "Pizza",
-    "Radio",
-    "Ruler",
-    "Sense",
-    "Slice",
-    "Space",
-    "Stamp",
-    "Taste",
-    "Toast",
-    "Voice",
-    "Waste",
-    "Watch",
-    "Wheel",
-    "Wife",
-    "Wine",
-    "Wrist",
-    "Xavier",
-    "Yellow",
-    "Zebra",
-    "Zesty",
-    "Braid",
+    "APPLE",
+    "BASIC",
+    "BLADE",
+    "BREAD",
+    "CLOUD",
+    "DANCE",
+    "FLUTE",
+    "GRAPE",
+    "IMAGE",
+    "KNIFE",
+    "LEMON",
+    "MOUSE",
+    "NURSE",
+    "OCEAN",
+    "PASTE",
+    "PIZZA",
+    "RADIO",
+    "RULER",
+    "SENSE",
+    "SLICE",
+    "SPACE",
+    "STAMP",
+    "TASTE",
+    "TOAST",
+    "VOICE",
+    "WASTE",
+    "WATCH",
+    "WHEEL",
+    "WRIST",
+    "ZESTY",
+    "BRAID",
 ];
 
 
+const word: string[] = []
 
-export function getRandomArrayString(array: string[]): string{
-    let randomIndex: number = Math.floor(Math.random() * array.length)
-    return array[randomIndex]
+const textArea = document.querySelector(".textArea") as HTMLDivElement;
+const doc = document;
+
+const cursor: Cursor = {
+    row: 0,
+    collum: 0,
+};
+
+doc.addEventListener("keypress", (e) => {
+    if (cursor.collum < 5 && "abcdefghijklmnopqrstuvwxyz".includes(e.key)) {
+        console.log(e.key);
+
+        const letter: string = e.key.toUpperCase();
+
+        textArea.children[cursor.row].children[cursor.collum].textContent = letter;
+
+        word.push(letter)
+        console.log(word)
+        console.log(word.join(""))
+
+        cursor.collum++;
+
+        
+    }
+});
+
+doc.addEventListener("keypress", (e) => {
+    
+    if(e.key === "Enter" && word.length === 5){
+        if(wordsArray.includes(word.join(""))){
+            cursor.row++
+            cursor.collum = 0
+
+            word.length = 0
+            console.log(word)
+        }
+    }
+});
+
+doc.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && cursor.collum > 0) {
+        cursor.collum--;
+
+        textArea.children[cursor.row].children[cursor.collum].textContent = "";
+
+        word.pop()
+        console.log(word)
+        console.log(word.join(""));
+    }
+});
+
+export function getRandomArrayString(array: string[]): string {
+    let randomIndex: number = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
 }
 
-
 export function getRandomWord(): string {
-    return getRandomArrayString(wordsArray)
+    return getRandomArrayString(wordsArray);
 }
