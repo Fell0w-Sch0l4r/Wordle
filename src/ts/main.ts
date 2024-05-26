4a421ad09dbc770e8bc898381ee018c9c66dfb51
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2024-05-26 23:57:49 +0100

    create alphabet array

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -38,6 +38,63 @@ export const wordsArray: string[] = [
 
 const word: string[] = []
 
+const ALFABET: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+];
+
+
+
 const textArea = document.querySelector(".textArea") as HTMLDivElement;
 const doc = document;
 
